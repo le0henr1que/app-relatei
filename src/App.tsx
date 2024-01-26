f6c5e77b4c9bd637dd6eb89a9310488e55f6c671
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2024-01-25 21:27:33 -0300

    feat: adicionando kutty para ds

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex space-x-3" aria-live="assertive" aria-atomic="true">
+        <div className="flex-none w-12 h-12 bg-gray-200 rounded-full animate-pulse"></div>
+        <div className="w-full space-y-2">
+          <div className="w-9/12 h-4 bg-gray-200 rounded animate-pulse"></div>
+          <div className="w-11/12 h-4 bg-gray-200 rounded animate-pulse"></div>
+          <div className="w-8/12 h-4 bg-gray-200 rounded animate-pulse"></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  plugins: [require('kutty')],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+};
