8c6995d7f06a9b31f7686a887ed70b08a567baf5
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2024-01-27 00:24:27 -0300

    fix: adicionando componets

--- src/modules/relato/home/Home.tsx
+++ src/modules/relato/home/Home.tsx
@@ -1,19 +1,46 @@
 import { Main } from '../../../shared/components/Main/Main';
+import { Card } from './components/card';
 import { Footer } from './components/footer';
 import { Header } from './components/header';
 
 export function Home() {
   return (
     <>
-      <Header />
+      <div className="flex flex-col min-h-screen">
+        <Header />
 
-      <Main>
-        <div className="container mx-auto my-8">
-          <h1 className="text-3xl font-bold">Card do relatei</h1>
-        </div>
-      </Main>
+        <Main>
+          <div className="w-full flex justify-center items-center mt-[84px] gap-[50px]">
+            <Card.Root>
+              <div className="flex flex-col gap-[24px] items-center justify-center">
+                <Card.Icon />
 
-      <Footer />
+                <Card.Content
+                  title="Criar Relato"
+                  description="Clique no botão abaixo para criar um relato anônimo ou identificado e envie-nos para podermos analisar-mos e ajudar você!"
+                />
+              </div>
+
+              <Card.Action />
+            </Card.Root>
+
+            <Card.Root>
+              <div className="flex flex-col gap-[24px] items-center justify-center">
+                <Card.Icon />
+
+                <Card.Content
+                  title="Acompanhar Relato"
+                  description="Digite abaixo o código do protocolo e clique no botão para acompanhar o andamento do seu relato"
+                />
+              </div>
+
+              <Card.Action />
+            </Card.Root>
+          </div>
+        </Main>
+
+        <Footer />
+      </div>
     </>
   );
 }
--- src/modules/relato/home/components/card/CardRoot.tsx
+++ src/modules/relato/home/components/card/CardRoot.tsx
@@ -1,1 +1,9 @@
-export function CardRoot() {}
+import { ReactNode } from 'react';
+
+export function CardRoot({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex flex-col items-center gap-[20px] w-[387px] p-[24px] border rounded-[15px] border-solid border-gray-300 justify-center">
+      {children}
+    </div>
+  );
+}
